4347791925caf4b4ca33e58910c610239589110a
Kiryl Kuchko <kuchkokirill@mail.ru>
2022-05-05 15:02:57 +0200

    Added sript for timer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
+    
+    // Tabs
 
     const tabs = document.querySelectorAll('.tabheader__item'),
             tabsContent = document.querySelectorAll('.tabcontent'),
@@ -39,4 +41,72 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+    
+    // Timer
+
+    const deadLine = '2022-05-07';
+
+    //foo calculate difference of actual time and deadline
+    function getTimeRemaining(endTime) {
+        const t = Date.parse(endTime) - Date.parse(new Date());
+        let days, hours, minutes, seconds;
+
+        if(t <= 0){
+            days = 0; 
+            hours = 0; 
+            minutes = 0; 
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24));
+            hours = Math.floor(t / (1000 * 60 * 60) % 24);
+            minutes = Math.floor((t / 1000 / 60) % 60);
+            seconds = Math.floor((t / 1000) % 60);
+        }
+
+        //key and value atomaticle assign ES6
+        return {
+            'total': t,
+            days,
+            hours,
+            minutes,
+            seconds
+        };
+    }
+
+    //function for zero below single digits
+    function getZero(num) {
+        if(num >= 0 && num < 10){
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endTime) {
+        const timer = document.querySelector(selector),
+                days = timer.querySelector('#days'),
+                hours = timer.querySelector('#hours'),
+                minutes = timer.querySelector('#minutes'),
+                seconds = timer.querySelector('#seconds'),
+                timeInterval = setInterval(updateClock, 1000); //updating timer function call every second 
+
+        updateClock(); //function call for avoid 1s delay in timeInterval on first iteration
+
+        //actualization timer by difference of actual time and deadline
+        function updateClock() {
+            const t = getTimeRemaining(endTime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            //stopper for timeInterval
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadLine);
 });
