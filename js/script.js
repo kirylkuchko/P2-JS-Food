509bd6b25a2d336dc6144dbdda1ddba726357f29
Kiryl Kuchko <kuchkokirill@mail.ru>
2022-06-15 10:56:18 +0200

    Added script for slider

--- js/script.js
+++ js/script.js
@@ -310,4 +310,54 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
+
+    // Slider
+
+    const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current');
+
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    } 
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach(item => item.style.display = 'none');
+
+        slides[slideIndex - 1].style.display = 'block';
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        } 
+    }
+
+    function slideCounterSum(n) {
+        showSlides(slideIndex += n);
+    }
+
+    next.addEventListener('click', () => {
+        slideCounterSum(1);
+    });
+
+    prev.addEventListener('click', () => {
+        slideCounterSum(-1);
+    });
 });
